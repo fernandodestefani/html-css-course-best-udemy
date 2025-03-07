d03565034ebc3b24a47302ae944c6c7ba9c5cb37
Fernando Destefani <henriqueorei@gmail.com>
2025-03-07 16:45:22 -0300

    final

--- 09-Omnifood-Optimizations/js/script.js
+++ 09-Omnifood-Optimizations/js/script.js
@@ -1,3 +1,85 @@
+console.log("Hello, world!")
+
+/* const myName = "Fernando Destefani"
+const h1 = document.querySelector(".heading-primary");
+console.log(h1);
+
+h1.addEventListener('click', function() {
+  h1.textContent = myName;
+  h1.style.backgroundColor = 'red';
+  h1.style.padding = "0.5em";
+}) */
+
+///////////////////////////////////////////////////////////
+// Set current year
+const yearEl = document.querySelector(".year");
+const currentYear = new Date().getFullYear();
+yearEl.textContent = currentYear; 
+
+///////////////////////////////////////////////////////////
+// Make mobile navigation work
+const btnNavEl = document.querySelector('.btn-mobile-nav');
+const headerEl = document.querySelector('.header');
+
+btnNavEl.addEventListener('click', function(){
+  headerEl.classList.toggle('nav-open');
+});
+
+///////////////////////////////////////////////////////////
+// Smooth scrolling navigation
+const allLinks = document.querySelectorAll('a:link');
+allLinks.forEach(function(link){
+  link.addEventListener('click', function(e){
+    e.preventDefault();
+    const href = link.getAttribute('href');
+
+    // scroll back to top
+    if (href === '#') 
+      window.scroll({
+        top: 0,
+        behavior: 'smooth'
+    });
+
+    // scroll to other links
+    if (href !== "#" && href.startsWith('#')) {
+      const sectionEl = document.querySelector(href);
+      sectionEl.scrollIntoView({behavior: 'smooth'})
+    }
+
+    // close mobile navigation
+    if (link.classList.contains('main-nav-link'))
+      headerEl.classList.toggle('nav-open');
+  });
+});
+
+
+///////////////////////////////////////////////////////////
+// Sticky navigation
+const sectionHeroEl = document.querySelector('.section-hero');
+const obs = new IntersectionObserver(function(entries) {
+  const ent = entries[0];
+  console.log(ent);
+
+  if (ent.isIntersecting === false) {
+    document.body.classList.add('sticky');
+  }
+
+  if (ent.isIntersecting === true) {
+    document.body.classList.remove('sticky');
+  }
+},
+{
+  //in the viewport
+  root: null,
+  threshold: 0,
+  rootMargin: '-80px',
+});
+obs.observe(sectionHeroEl);
+
+
+
+
+
 ///////////////////////////////////////////////////////////
 // Fixing flexbox gap property missing in some Safari versions
 function checkFlexGap() {
